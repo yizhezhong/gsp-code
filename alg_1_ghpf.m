79a758f86690ecb867cc7d6c4a19da739abeb60a
Yizhe Zhong <yz251384@dal.ca>
2023-11-03 07:24:56 -0300

    break symmetry

--- alg_1_ghpf.m
+++ alg_1_ghpf.m
@@ -33,15 +33,36 @@ function [phi, tor, return_cutoff, cutoff] = alg_1_ghpf(L, signals, e, historic_
             for m = r:M
                 sum = sum + (dot(F, eig_vec(:,m)))^2;
             end
-            sum
+            %sum
             if e < sum
                 cutoff = r;
                 return_cutoff = eig_val(r,r);
                 break
             end
         end
-        cutoff
-        diag(Lambda)
+        %cutoff
+        %diag(Lambda)
+
+        % ---- this is for sum vs. index comparison ----
+        for r = M:-1:1 %step 4
+            sum2 = 0;
+            for m = r:M
+                sum2 = sum2 + (dot(F, eig_vec(:,m)))^2;
+            end
+            error_sum(r) = sum2;
+            %sum
+            r
+            sum2
+        end
+        
+        figure(10)
+        plot(error_sum, '-o')
+        % ---- this is the end ----
+
+        %set cutoff manually
+        cutoff = round(M * 0.6)
+        return_cutoff = eig_val(cutoff,cutoff);
+
         H = GHPF(L, cutoff);
         Fs = H*F;
         Fs
