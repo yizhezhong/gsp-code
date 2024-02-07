39f1beb04738e59f9fb09b54b5fd50c1d45ace02
yizhe_msi <yizhezhong@outlook.com>
2024-02-07 00:40:13 -0400

    grid case

--- break_symmetry.m
+++ break_symmetry.m
@@ -9,23 +9,23 @@ function new_G = break_symmetry(G, graph_size, break_amount)
         if mod(i,2) == 0
             counter_1 = counter_1 + 1;
             new_G.A(numRows+i,numCols+i) = 0;
-            new_G.A(numRows+i,numCols+i-2*counter_1) = 1;
+            new_G.A(numRows+i,numCols+i-2*counter_1-4) = 1;
             %[numRows2,numCols2] = size(new_G.A)
             new_G = grasp_symetrise_unweighted(new_G);
-            new_G.layout(numRows+i, 1) = new_G.layout(numRows,1);
-            new_G.layout(numRows+i, 2) = new_G.layout(numRows,2) + sqrt(3)*(counter_1);
+            new_G.layout(numRows+i, 1) = new_G.layout(numRows,1)-1;
+            new_G.layout(numRows+i, 2) = new_G.layout(numRows,2) + sqrt(3)*(counter_1)+1;
         elseif mod(i,2) == 1
             counter_2 = counter_2 + 1;
             new_G.A(numRows+i,numCols+i) = 0;
-            new_G.A(numRows+i,numCols+i-2*counter_2) = 1;
+            new_G.A(numRows+i,numCols+i-2*counter_2-4) = 1;
             %[numRows2,numCols2] = size(new_G.A)
             new_G = grasp_symetrise_unweighted(new_G);
-            new_G.layout(numRows+i, 1) = new_G.layout(numRows-1,1);
-            new_G.layout(numRows+i, 2) = new_G.layout(numRows-1,2) + sqrt(3)*(counter_2);
+            new_G.layout(numRows+i, 1) = new_G.layout(numRows-1,1)-1;
+            new_G.layout(numRows+i, 2) = new_G.layout(numRows-1,2) + sqrt(3)*(counter_2)+1;
         end
     end
     new_G.A(numRows+break_amount,numCols+break_amount-1) = 1;
     [numRows,numCols] = size(new_G.A);
-    new_G.A(numRows, total_nodes-1) = 1;
+    %new_G.A(numRows, total_nodes-1) = 1;
     new_G = grasp_symetrise_unweighted(new_G);
 end
